692da3cee4bc5a602f7daa807b87a1d0e4e612a3
anoojshrestha <anoojshrestha.52532@gmail.com>
2019-12-29 14:45:45 +0100

    Updates

---
+++ precache-manifest.25e0fba9e405aaa37bf46346c4c3fbdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee9195d05379886bb0ae632b1c664bf",
+    "url": "/planner/index.html"
+  },
+  {
+    "revision": "1bd07980150b1ba2988e",
+    "url": "/planner/static/css/main.f4d37901.chunk.css"
+  },
+  {
+    "revision": "5b3586fffdc22e20f04d",
+    "url": "/planner/static/js/2.b7d744d8.chunk.js"
+  },
+  {
+    "revision": "1bd07980150b1ba2988e",
+    "url": "/planner/static/js/main.27bfa269.chunk.js"
+  },
+  {
+    "revision": "4d12205ebdd545808bdc",
+    "url": "/planner/static/js/runtime-main.d7d6ab39.js"
+  }
+]);
